77dfbf6661743d14b87a1949e77989ff55296588
teadave <gorski.dave@gmail.com>
2017-09-28 18:37:38 -0400

    Completed Resize assignment
    Realized that photos are block-aligned so I can just blindly copy whole
    blocks and not subtract from 512 to copy parts of them. Completed pset4.

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -1,15 +1,26 @@
 #include "stdio.h"
 #include "stdbool.h"
+#include "stdlib.h"
 
+
+//Check if the bit is set
 bool is_bit_set(unsigned char c, int n)
 {
     return (1 & (c >> n));
 }
 
+//Generate a file name with three digits
+void generate_filename(char* filename, int counter)
+{
+    if(counter < 10)
+        sprintf(filename, "00%d.jpeg", counter);
+    else if(counter < 100)
+        sprintf(filename, "0%d.jpeg", counter);
+    else if(counter < 1000)
+        sprintf(filename, "%d.jpeg", counter);
+}
 
-
-
-
+//Main functions
 int main(int argc, char* argv[])
 {
     //Check command line arguments
@@ -34,68 +45,60 @@ int main(int argc, char* argv[])
 
    //Prepare buffer
    char buffer[512] = {};
-    
+   
+   //Count for filenames 
    int counter = 0;
 
+   //Is a file currently open
    bool fileopen = false;
-
-
 
    //Output file pointer
    FILE *output_file;
     
    //New file name
-   char* new_filename[10];
-   sprintf(new_filename, "00%d.jpeg", counter);
-   
+   char* new_filename = malloc(sizeof(char) * 10);
+   generate_filename(new_filename, counter);
 
    //Read in one block at a time
    while(fread(buffer, 1, 512, input_file) == 512)
-   {
-       for(int i = 0; i < 512; i++)
-       {    
+   { 
 
-
-           //Check if start of file
-           if( (buffer[i] == (char)0xff) && 
-             (buffer[i+1] == (char)0xd8) &&
-             (buffer[i+2] == (char)0xff) &&
-             (is_bit_set(buffer[i+3],7)) && 
-             (is_bit_set(buffer[i+3],6)) &&
-             (is_bit_set(buffer[i+3],6)))
-           {
-               
-               if(!fileopen)
-               {
-                   printf("CREATING NEW FILE\n");
-                   sprintf(new_filename,"00%d.jpeg", counter);
-                   output_file = fopen(new_filename, "w");
-                   fileopen = true; 
-               }
-               else
-               {
-
-
-                   fwrite(buffer, i, 1, output_file), 
-                   printf("ClOSING FILE\n");
-                   fclose(output_file);
-                   fileopen = false;
-               }
-               
-               counter++;
-
-           }
+      //Check if start of file
+       if(( (buffer[0] == (char)0xff) && 
+         (buffer[1] == (char)0xd8) &&
+         (buffer[2] == (char)0xff) &&
+         (is_bit_set(buffer[3],7)) && 
+         (is_bit_set(buffer[3],6)) &&
+         (is_bit_set(buffer[3],6))))
+       {
            
-
+           //If file open close it 
+           if(fileopen)
+           {
+               fclose(output_file);
+               fileopen = false;
+           }   
+           
+           //Create New file
+           generate_filename(new_filename, counter);
+           output_file = fopen(new_filename, "w");
+           fileopen = true;
+           counter++;
+          
        }
+       
+       //Write out block 
        if(fileopen)
        {
            fwrite(buffer, 512, 1, output_file);
-           printf("WROTE BLOCK\n");
        }
    }
-
-   printf("Number of Files: %d\n", counter);
+   
+   
+   //Free memory used for string  
+   free(new_filename);
+   
+   //Close input file
    fclose(input_file);
 
 
